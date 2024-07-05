8fed9a93a3db522b70ebcc7a5fa929483fd411da
leonrom <romleon@gmail.com>
2024-07-05 11:57:56 +0300

    1

--- o5ref..js
+++ o5ref..js
@@ -2,7 +2,7 @@
 /*jshint asi:true  */
 /*jshint esversion: 6*/
 /*eslint no-useless-escape: 0*/
-(function () {                // 3---------------------------------------------- o5ref ---
+(function () {                  // 3---------------------------------------------- o5ref ---
 	'use strict';
 
 	const
